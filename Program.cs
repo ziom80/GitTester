534007245d392d57d072b680da588fc8611268fd
marcinruszczyk <ruszmar@gmail.com>
2025-03-03 10:46:52 +0100

    Zmiany cde

--- Program.cs
+++ Program.cs
@@ -9,6 +9,9 @@ namespace GitTester
             Console.WriteLine("Hello World!");
             //branch master
             //zmiany branch b
+            //zmiany branch c
+            //zmiana d
+            //zmiana e
         }
     }
 }
